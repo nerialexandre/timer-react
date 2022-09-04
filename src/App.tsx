5a373ee3106aa5cc8646d102091a9bdd4d0a8b48
Alexandre Neri <carlosalexandre.neri@gmail.com>
2022-09-03 21:42:32 -0300

    add styled-components

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/Button/Button'
+import { ThemeProvider} from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant='primary' />
+      <Button variant='secondary' />
+      <Button variant='danger' />
+      <Button variant='success' />
+    </ThemeProvider>
+  )
+}
